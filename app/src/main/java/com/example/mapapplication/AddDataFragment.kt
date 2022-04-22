8c2b278afbc285e64947fa492ddfc8f352e76c9c
NethminiDevyanjalee <73274982+NethminiDevyanjalee@users.noreply.github.com>
2022-04-23 00:19:21 +0530

    implemented data saving in the local db

--- app/src/main/java/com/example/mapapplication/AddDataFragment.kt
+++ app/src/main/java/com/example/mapapplication/AddDataFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.mapapplication.database.AppDatabase
 import com.example.mapapplication.databinding.FragmentAddDataBinding
+import com.example.mapapplication.model.Location
 
 
 class AddDataFragment : Fragment() {
@@ -29,6 +31,9 @@ class AddDataFragment : Fragment() {
 
         binding.btnSave.setOnClickListener {
             findNavController().navigate(R.id.action_addDataFragment_to_viewFragment)
+            var location = Location(binding.editTextName.text.toString(),binding.editTextLatitude.text.toString(),binding.editTextLongitude.text.toString())
+            val db = AppDatabase.getDatabase(view.context)
+            db.locationDao().insert(location)
         }
 
     }
